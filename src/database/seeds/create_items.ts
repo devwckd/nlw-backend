de4a177fa6bdcaea32416cfff6586146db0b87fa
devwckd <dev.wckd@gmail.com>
2020-06-02 15:16:46 -0300

    Created items route with database support.

---
+++ src/database/seeds/create_items.ts
@@ -0,0 +1,24 @@
+import Knex from 'knex';
+
+export async function seed(knex: Knex) {
+    await knex('items').insert([
+        {
+            title: 'Lâmpadas', image: 'lampadas.svg'
+        },
+        {
+            title: 'Pilhas e baterias', image: 'baterias.svg'
+        },
+        {
+            title: 'Papéis e papelão', image: 'papeis-papelao.svg'
+        },
+        {
+            title: 'Resíduos Eletrônicos', image: 'eletronicos.svg'
+        },
+        {
+            title: 'Resíduos Orgânicos', image: 'organicos.svg'
+        },
+        {
+            title: 'Óleo de Cozinha', image: 'oleo.svg'
+        },
+    ]);
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import knex from './database/connection';
 
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-    return response.json({ message: "NLW RestAPI index." })
+routes.get('/items', async (request, response) => {
+    const items = await knex('items').select('*');
+    return response.json(items);
 })
 
 export default routes;
